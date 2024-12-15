a2d91b8b034f4f9688ec34da12e43aa33f737e85
Connor Kress <ckress@ufl.edu>
2024-12-14 23:26:32 -0500

    Center map on location and add user icon

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,73 @@
-import { LatLngExpression } from 'leaflet';
+import { LatLngExpression, Icon } from 'leaflet';
 import {
   MapContainer,
+  Marker,
   TileLayer,
-} from 'react-leaflet'
+  useMap,
+} from 'react-leaflet';
 
-import 'leaflet/dist/leaflet.css'
-import './App.css'
+import 'leaflet/dist/leaflet.css';
+import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
-  const centerPos: LatLngExpression = [29.648643, -82.349709];
+// https://www.flaticon.com/free-icon/rhombus_10239023?term=diamond+shape&page=1&position=25&origin=search&related_id=10239023
+const userIcon = new Icon({
+  iconUrl: "/diamond.png",
+  iconSize: [24, 30],
+  iconAnchor: [12, 15],
+  className: "cursor-default",
+});
+
+export default function App() {
+  const [userPos, setUserPos] = useState<LatLngExpression | null>(null);
+
+  useEffect(() => {
+    const watchId = navigator.geolocation.watchPosition(
+      (pos) => {
+        console.log("User location:",
+                    [pos.coords.latitude, pos.coords.longitude])
+        setUserPos([pos.coords.latitude, pos.coords.longitude]);
+      },
+      (error) => {
+        console.log("Geolocation error:", error);
+      },
+      { enableHighAccuracy: true, maximumAge: 0, timeout: 5000 },
+    );
+
+    return () => navigator.geolocation.clearWatch(watchId); // cleanup
+  }, []);
+
+  if (!userPos) {
+    return <h2>Locating Position...</h2>;
+  }
 
   return (
-    <MapContainer style={{height: "100vh"}} center={centerPos}
-                  zoom={16} scrollWheelZoom={false}>
+    <MapContainer
+      style={{height: "100vh"}}
+      center={userPos}
+      zoom={16}
+      dragging={false}
+      zoomControl={false}
+      doubleClickZoom={false}
+      scrollWheelZoom={false}
+      keyboard={false}
+      boxZoom={false}
+      touchZoom={false}
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <MapUpdater center={userPos} />
+      <Marker position={userPos} icon={userIcon} />
     </MapContainer>
   );
 }
 
-export default App
+function MapUpdater({ center }: { center: LatLngExpression }) {
+  const map = useMap(); // gets the closest parent <MapContainer />
+  useEffect(() => {
+    map.setView(center);
+  }, [center, map]);
+  return null;
+}
