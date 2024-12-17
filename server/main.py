a9d217a608d79b898752428e56d78149555a25fa
Connor Kress <ckress@ufl.edu>
2024-12-17 00:07:29 -0500

    Added basic `ResourceNode` class and mock data creation

--- server/main.py
+++ server/main.py
@@ -1,13 +1,62 @@
+import random
+from enum import Enum
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from util import to_camel_case
 
 app = FastAPI()
 
 
+class NodeType(str, Enum):
+    TREE = "tree"
+    ROCK_BASIC = "rockBASIC"
+    ROCK_COPPER = "rockCopper"
+    ROCK_IRON = "rockIron"
+
+
+NODE_TYPE_WEIGHTS: dict[NodeType, int] = {
+    NodeType.TREE: 100,
+    NodeType.ROCK_BASIC: 40,
+    NodeType.ROCK_COPPER: 25,
+    NodeType.ROCK_IRON: 15,
+}
+
+
+def get_random_node_type() -> NodeType:
+    types = list(NODE_TYPE_WEIGHTS.keys())
+    weights = list(NODE_TYPE_WEIGHTS.values())
+    return random.choices(types, weights=weights, k=1)[0]
+
+
+class ResourceNode(BaseModel):
+    id: int
+    node_type: NodeType
+    lat: float = Field(ge=-90, le=90)  # -90 <= lat <= 90
+    lon: float = Field(ge=-180, le=180)  # -180 <= lon <= 180
+
+    class Config:
+        alias_generator = to_camel_case
+        populate_by_name = True
+        allow_population_by_field_name = True
+
+
 @app.get("/api")
-async def read_root():
-    return {"message": "Hello, World!"}
+async def get_root():
+    return {"message": "This is an API"}
 
 
-@app.get("/api/items/{item_id}")
-async def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "query": q}
+@app.get("/api/nodes")
+async def get_nodes(lat: float, lon: float, radius: float) -> list[ResourceNode]:
+    # Convert from meters to degrees latitude/longitude
+    lat_offset = radius / 111_111
+    lon_offset = radius / 111_321
+    # Mock data
+    return [
+        ResourceNode(
+            id=int(random.randint(0, 1000000)),
+            node_type=get_random_node_type(),
+            lat=lat + lat_offset*(random.random()-0.5)*2,
+            lon=lon + lon_offset*(random.random()-0.5)*2,
+        )
+        for _ in range(10)
+    ]
