e8d26e235c364d0a50e8ec6eea1cb8d37081336e
Connor Kress <ckress@ufl.edu>
2025-01-02 19:14:15 -0500

    Moved `<GameMap />` into its own file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,8 @@
-import {
-  MapContainer,
-  Marker,
-  Popup,
-  TileLayer,
-  useMap,
-} from 'react-leaflet';
-import { useEffect } from 'react';
-import { userIcon, getResourceIcon } from "./icons"
-import { Coords, ResourceNode } from "./types"
-
-import 'leaflet/dist/leaflet.css';
-import './App.css';
 import { useLocation } from './hooks/useLocation';
 import { useResourceNodes } from './hooks/useResourceNodes';
 import { GenRandomNodeButton } from './components/GenRandomNodeButton';
+import { GameMap } from './components/GameMap';
+import './App.css';
 
 export default function App() {
   const { position } = useLocation();
@@ -31,76 +20,4 @@ export default function App() {
       <GameMap position={position} nodes={nodes} />
     </>
   );
-}
-
-async function copyCoordsToClipboard(coords: Coords) {
-  const coordsMsg = `${coords.lat}, ${coords.lon}`;
-  await navigator.clipboard.writeText(coordsMsg);
-}
-
-function GameMap(props: {
-  position: Coords, nodes: ResourceNode[]
-}) {
-  const { position, nodes } = props;
-
-  return (
-    <MapContainer
-      style={{height: "100vh"}}
-      center={[position.lat, position.lon]}
-      zoom={17}
-      dragging={false}
-      zoomControl={false}
-      doubleClickZoom={false}
-      scrollWheelZoom={false}
-      keyboard={false}
-      boxZoom={false}
-      touchZoom={false}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <MapUpdater center={position} />
-      <Marker position={[position.lat, position.lon]} icon={userIcon}>
-        <Popup>
-          User<br/>
-          lat: {position.lat.toFixed(5)}{" "}
-          lon: {position.lon.toFixed(5)}
-          <br/>
-          <button onClick={async () => {
-            await copyCoordsToClipboard(position);
-          }}>
-            Copy to Clipboard
-          </button>
-        </Popup>
-      </Marker>
-      {nodes.map((node, idx) => (
-        <Marker
-          position={[node.coords.lat, node.coords.lon]}
-          icon={getResourceIcon(node.nodeType)}
-          key={idx}
-        >
-          <Popup>
-            {node.nodeType}<br/>
-            lat: {node.coords.lat.toFixed(5)}{" "}
-            lon: {node.coords.lon.toFixed(5)}
-            <br/>
-            <button onClick={async () => {
-              await copyCoordsToClipboard(node.coords);
-            }}>
-              Copy to Clipboard
-            </button>
-          </Popup>
-        </Marker>
-      ))}
-    </MapContainer>
-  );
-}
-
-function MapUpdater({ center }: { center: Coords }) {
-  const map = useMap(); // gets the closest parent <MapContainer />
-  useEffect(() => {
-    map.setView([center.lat, center.lon]);
-  }, [center, map]);
-  return null;
 }
---
+++ client/src/components/GameMap.tsx
@@ -0,0 +1,77 @@
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+import { Coords, ResourceNode } from "../types";
+import { getResourceIcon, userIcon } from "../icons";
+import { useEffect } from "react";
+import 'leaflet/dist/leaflet.css';
+
+async function copyCoordsToClipboard(coords: Coords) {
+  const coordsMsg = `${coords.lat}, ${coords.lon}`;
+  await navigator.clipboard.writeText(coordsMsg);
+}
+
+export function GameMap(props: {
+  position: Coords, nodes: ResourceNode[]
+}) {
+  const { position, nodes } = props;
+
+  return (
+    <MapContainer
+      style={{height: "100vh"}}
+      center={[position.lat, position.lon]}
+      zoom={17}
+      dragging={false}
+      zoomControl={false}
+      doubleClickZoom={false}
+      scrollWheelZoom={false}
+      keyboard={false}
+      boxZoom={false}
+      touchZoom={false}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <MapUpdater center={position} />
+      <Marker position={[position.lat, position.lon]} icon={userIcon}>
+        <Popup>
+          User<br/>
+          lat: {position.lat.toFixed(5)}{" "}
+          lon: {position.lon.toFixed(5)}
+          <br/>
+          <button onClick={async () => {
+            await copyCoordsToClipboard(position);
+          }}>
+            Copy to Clipboard
+          </button>
+        </Popup>
+      </Marker>
+      {nodes.map((node, idx) => (
+        <Marker
+          position={[node.coords.lat, node.coords.lon]}
+          icon={getResourceIcon(node.nodeType)}
+          key={idx}
+        >
+          <Popup>
+            {node.nodeType}<br/>
+            lat: {node.coords.lat.toFixed(5)}{" "}
+            lon: {node.coords.lon.toFixed(5)}
+            <br/>
+            <button onClick={async () => {
+              await copyCoordsToClipboard(node.coords);
+            }}>
+              Copy to Clipboard
+            </button>
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+  );
+}
+
+function MapUpdater({ center }: { center: Coords }) {
+  const map = useMap(); // gets the closest parent <MapContainer />
+  useEffect(() => {
+    map.setView([center.lat, center.lon]);
+  }, [center, map]);
+  return null;
+}
