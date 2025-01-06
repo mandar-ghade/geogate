8c9972ff8fa3ae8720c0121bfc17a08ce0a075d0
Connor Kress <ckress@ufl.edu>
2025-01-05 23:09:00 -0500

    Initial user/session table schema

--- server/db/migrations/002_users_and_sessions.sql
+++ server/db/migrations/002_users_and_sessions.sql
@@ -1,15 +1,3 @@
-CREATE EXTENSION IF NOT EXISTS postgis;
-
-CREATE TABLE resource_nodes (
-    id SERIAL PRIMARY KEY,
-    node_type TEXT NOT NULL,
-    location GEOGRAPHY(Point, 4326),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
--- Add a spatial index to optimize geospatial queries
-CREATE INDEX idx_location ON resource_nodes USING GIST(location);
-
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(255) NOT NULL UNIQUE,
